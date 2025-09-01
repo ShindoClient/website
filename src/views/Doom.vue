f23ce7d03111f4bfe9c965ee52f0fb0915ebad9c
MikiDevAHM <st_git_miki@netstat.com.br>
2025-09-01 13:48:39 +0200

    Updated

--- src/views/Doom.vue
+++ src/views/Doom.vue
@@ -1,32 +1,8 @@
 <!-- Doom.vue -->
 <template>
-  <div class="ratio">
-    <iframe
-        src="https://archive.org/embed/doom-shareware-v1.666"
-        frameborder="0"
-        allowfullscreen
-        scrolling="no"
-        referrerpolicy="no-referrer-when-downgrade"
-    />
-  </div>
+  <iframe
+      src="https://raz0red.github.io/webprboom/"
+      style="width:100%; height:80vh; border:0;"
+      allow="fullscreen"
+  />
 </template>
-
-<script setup lang="ts">
-// nada necessário aqui :)
-</script>
-
-<style scoped>
-/* Mantém o iframe responsivo (4:3) */
-.ratio {
-  position: relative;
-  width: 100%;
-  padding-top: 75%;
-}
-.ratio > iframe {
-  position: absolute;
-  inset: 0;
-  width: 100%;
-  height: 100%;
-  border: 0;
-}
-</style>
